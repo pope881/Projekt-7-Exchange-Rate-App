ee252430a47bd085c040f6fedcb4496bb38d7239
Bartosz Papierz <papierz.bartosz@gmail.com>
2023-01-03 19:56:41 +0100

    created new app

---
+++ script.js
@@ -0,0 +1,32 @@
+const currencyOne = document.querySelector('#currency-one')
+const amountOne = document.querySelector('.amount-one')
+const currencyTwo = document.querySelector('#currency-two')
+const amountTwo = document.querySelector('.amount-two')
+const swapBtn = document.querySelector('.swap')
+const rateInfo = document.querySelector('.rate-info')
+
+const calculete = () => {
+	fetch(`https://api.exchangerate.host/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`)
+		.then(res => res.json())
+		.then(data => {
+			const currency1 = currencyOne.value
+			const currency2 = currencyTwo.value
+			const rate = data.rates[currency2]
+
+			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`
+			amountTwo.value = (amountOne.value * rate).toFixed(2)
+		})
+}
+
+const swap = () => {
+    const oldValue = currencyOne.value
+    currencyOne.value = currencyTwo.value
+    currencyTwo.value = oldValue
+    calculete()
+}
+
+currencyOne.addEventListener('change', calculete)
+currencyTwo.addEventListener('change', calculete)
+amountOne.addEventListener('input', calculete)
+swapBtn.addEventListener('click', swap)
+calculete()
